6f621db69324a58e97220b5fd30407bfe53f87fe
Maksim Torgashin <soulscavenger@yandex.ru>
2025-01-31 03:13:52 +0300

    ?

--- backend/recipes/management/commands/fill_db.py
+++ backend/recipes/management/commands/fill_db.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
         os.chdir(DIR)
         path_to_ingr = os.getcwd()
         path_to_ingredients = os.path.join(path_to_ingr, 'ingredients.json')
-        path_to_tags = os.path.join(path_to_ingr, 'tags.json')
+        # path_to_tags = os.path.join(path_to_ingr, 'tags.json')
 
         with open(path_to_ingredients, 'r', encoding='UTF-8') as file:
             ingredients = json.load(file)
@@ -25,11 +25,11 @@ class Command(BaseCommand):
             except Exception as error:
                 print(f"Что-то пошло не так. {error}")
 
-        with open(path_to_tags, 'r', encoding='UTF-8') as file:
-            ingredients = json.load(file)
+        # with open(path_to_tags, 'r', encoding='UTF-8') as file:
+        #     ingredients = json.load(file)
 
-        for ingredient in ingredients:
-            try:
-                Ingredient.objects.get_or_create(**ingredient)
-            except Exception as error:
-                print(f"Что-то пошло не так. {error}")
+        # for ingredient in ingredients:
+        #     try:
+        #         Ingredient.objects.get_or_create(**ingredient)
+        #     except Exception as error:
+        #         print(f"Что-то пошло не так. {error}")
