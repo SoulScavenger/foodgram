2e61b5041ad2cc67c7a4d2b6bac78ae20a409c8a
Maksim Torgashin <soulscavenger@yandex.ru>
2025-01-31 03:07:06 +0300

    final version

--- backend/core/constants.py
+++ backend/core/constants.py
@@ -26,3 +26,4 @@ MIN_COCKING_TIME = 1
 MAX_VIEW_LENGTH = 30
 URL = 'http://soulscavengerkitty.ddns.net/s/'
 SHORT_LINK_MAX_POSTFIX = 10
+DIR = '../data/'
--- backend/recipes/management/commands/fill_db.py
+++ backend/recipes/management/commands/fill_db.py
@@ -1,19 +1,22 @@
 import json
+import os
 
 from django.core.management.base import BaseCommand
 
-from recipes.models import Ingredient, Tag
-
-from foodgram.settings import JSON_FILES_DIR
+from core.constants import DIR
+from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
     """Загрузка ингридиентов в БД."""
 
     def handle(self, *args, **options):
-        path_to_json_file_ingr = f'{JSON_FILES_DIR}/ingredients.json'
-        path_to_json_file_tags = f'{JSON_FILES_DIR}/tags.json'
-        with open(path_to_json_file_ingr, 'r', encoding='UTF-8') as file:
+        os.chdir(DIR)
+        path_to_ingr = os.getcwd()
+        path_to_ingredients = os.path.join(path_to_ingr, 'ingredients.json')
+        path_to_tags = os.path.join(path_to_ingr, 'tags.json')
+
+        with open(path_to_ingredients, 'r', encoding='UTF-8') as file:
             ingredients = json.load(file)
 
         for ingredient in ingredients:
@@ -22,11 +25,11 @@ class Command(BaseCommand):
             except Exception as error:
                 print(f"Что-то пошло не так. {error}")
 
-        with open(path_to_json_file_tags, 'r', encoding='UTF-8') as file:
-            tags = json.load(file)
+        with open(path_to_tags, 'r', encoding='UTF-8') as file:
+            ingredients = json.load(file)
 
-        for tag in tags:
+        for ingredient in ingredients:
             try:
-                Tag.objects.get_or_create(**tag)
+                Ingredient.objects.get_or_create(**ingredient)
             except Exception as error:
                 print(f"Что-то пошло не так. {error}")
